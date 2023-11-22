7efd5a4d8784cf2d434b1b2b86345f3410f1d9d9
TJonCanon <taylor.joyner93@me.com>
2023-11-21 18:57:11 -0600

    task 2

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,93 +1,52 @@
 // graphql application for project/task organization
 // many comments are relics of local storage
 const graphql = require('graphql');
-const {
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLSchema,
-  GraphQLInt,
-  GraphQLID,
-  GraphQLList,
-  GraphQLNonNull,
-} = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLInt } = graphql;
 const _ = require('lodash');
-const Task = require('../models/task');
-const Project = require('../models/project');
 
-// defines tasks for graphql
+const tasks = [
+  {
+    id: '1',
+    title: 'Create your first webpage',
+    weight: '1',
+    description:
+      'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+  },
+  {
+    id: '2',
+    title: 'Structure your webpage',
+    weight: '1',
+    description:
+      'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+  },
+];
+
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: () => ({
-    id: { type: GraphQLID },
+    id: { type: GraphQLString },
     title: { type: GraphQLString },
     weight: { type: GraphQLInt },
     description: { type: GraphQLString },
-    project: {
-      type: ProjectType,
-      resolve(parent, args) {
-        // return _.find(projects, { id: parent.projectId });
-        return Project.findById(parent.projectId);
-      },
-    },
   }),
 });
 
-// defines projects for graphql
-const ProjectType = new GraphQLObjectType({
-  name: 'Project',
-  fields: () => ({
-    id: { type: GraphQLID },
-    title: { type: GraphQLString },
-    weight: { type: GraphQLInt },
-    description: { type: GraphQLString },
-    tasks: {
-      type: new GraphQLList(TaskType),
-      resolve(parent, args) {
-        // return _.filter(tasks, { projectId: parent.id });
-        return Task.findById({ projectId: parent.id });
-      },
-    },
-  }),
-});
-
-// defines default RootQuery for graphql
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: () => ({
     task: {
       type: TaskType,
       args: {
-        id: { type: GraphQLID },
+        id: { type: GraphQLString },
       },
       resolve(parent, args) {
-        // return _.find(tasks, { id: args.id });
-        return Task.findById(args.id);
-      },
-    },
-    project: {
-      type: ProjectType,
-      args: {
-        id: { type: GraphQLID },
-      },
-      resolve(parent, args) {
-        // return _.find(projects, { id: args.id });
-        return Project.findById(args.id);
-      },
-    },
-    tasks: {
-      type: new GraphQLList(TaskType),
-      resolve(parent, args) {
-        // return tasks;
-        return Task.find({});
-      },
-    },
-    projects: {
-      type: new GraphQLList(ProjectType),
-      resolve(parent, args) {
-        // return projects;
-        return Project.find({});
+        // code to get data from db / other source
+        return _.find(tasks, { id: args.id });
       },
     },
   }),
 });
 
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
